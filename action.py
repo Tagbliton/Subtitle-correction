122dd21c319c49100db1fd3da0a25ac914e922bb
Tagbliton <153424240+Tagbliton@users.noreply.github.com>
2025-06-13 18:08:06 +0800

    Update action.py

--- action.py
+++ action.py
@@ -1,9 +1,9 @@
 import json
 from config import api_key
 from openai import OpenAI
+from deepseek_v3_tokenizer.deepseek_tokenizer import tokenizer
 
-
-
+#提取文本
 def extract_text_from_srt(input_file):
     output_file='output.txt'
     with open(input_file, 'r', encoding='utf-8') as file:
@@ -19,7 +19,7 @@ def extract_text_from_srt(input_file):
     return "字幕文本已提取至output.txt", "output.txt"
 
 
-
+#切分字幕
 def split_file(max_lines):
     input_file_path = "output.txt"
     output_prefix = "results"
@@ -43,7 +43,7 @@ def split_file(max_lines):
 
     return f"发现{total_lines}条字幕.\n{message}"
 
-
+#一键提取并切分
 def action(input_file, max_lines):
     output1, output2=extract_text_from_srt(input_file)
     output3=split_file(max_lines)
@@ -52,14 +52,23 @@ def action(input_file, max_lines):
 
 
 
-
+#读取文本
 def read_file_to_variable(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         A = file.read()
     return A
 
+#保存文件
+def save_file(file, value):
+    with open(file, 'w', encoding='utf-8') as f:
+        f.write(value)
+    print(f"内容已成功写入 {file}.txt 文件。")
+    return f"内容已成功写入 {file}.txt 文件。"
 
-
+#计算token
+def token(index):
+    value = read_file_to_variable(f"results_{index}.txt")
+    return f"{tokenizer(value)}/64000"
 
 
 
@@ -119,17 +128,12 @@ def read_file_to_variable(file_path):
 
 
 
-#deepseek
-
 
 
 #系统提示词处理
-
-
-
 def system_prompt(index, cut_lines):
-    lines = int(index) * int(cut_lines) + 1
-    prompt = f'用户将提供一些视频字幕文本，序号从{lines}开始标注，校正其中明显的错别字，有些内容虽然表述错误但可能符合视频人物说出的话，修改其中明显的错别字，请解析为"before"和"after"并以json格式输出.'
+    lines = int(index-1) * int(cut_lines) + 1
+    prompt = f'用户将提供一些视频字幕文本，序号从{lines}开始标注，校正其中明显的错别字，有些内容虽然表述错误但可能符合视频人物说出的话，请将"修改前"、"确定的错误"、"可能的错误"以及"修改后"分别解析为"before"、"error"、"maybe"和"after"，并以json格式输出.'
     example = """
     示例输入: 
     183. 这个项目占不占自然岸线
@@ -167,25 +171,42 @@ def system_prompt(index, cut_lines):
         194. 已经实施了
         195. 那么下阶段这个自然案线呢
         196. 也是海洋督查
-        197. 呃这
-        198. 个督查的一个重点啊},
+        197. 呃这个
+        198. 督查的一个重点啊
+        },
+        "error": {
+        187. 那么案线保有率的管控啊  // 案线 -> 岸线 //（海岸线术语）
+        188. 这个国家对于我们自然案线的保有率  // 案线 -> 岸线
+        190. 呃三审仪式啊  // 三审仪式 -> 三省一市 //（指上海、江苏、浙江、福建）
+        191. 东海区的三审仪式呢  // 三审仪式 -> 三省一市
+        192. 这个新修测案线的这个成果  // 案线 -> 岸线
+        195. 那么下阶段这个自然案线呢  // 案线 -> 岸线
+        196. 也是海洋督查  // 督查 -> 督察 //（行政监督术语）
+        198. 督查的一个重点啊  // 督查 -> 督察
+        },
+        "maybe": {
+        185. 一个一个  //  一个
+        190. 呃三省一市啊  //  三省一市啊
+        193. 已经进入经各个省市人民政府的批准  //  已经经各个省市人民政府的批准
+        },
         "after": {
         183. 这个项目占不占自然岸线
-        184. 基本上的分类的
+        184. 基本上的分类
         185. 一个
-        186. 一个简单的一个现场认定标准啊
+        186. 一个简单的现场认定标准啊
         187. 那么岸线保有率的管控啊
         188. 这个国家对于我们自然岸线的保有率
         189. 其实是有要求的
         190. 三省一市啊
         191. 东海区的三省一市呢
-        192. 这个新修测岸线的这个成果
-        193. 已经进入经各个省市人民政府的批准
+        192. 这个新修测案线的成果
+        193. 已经经各个省市人民政府的批准
         194. 已经实施了
         195. 那么下阶段这个自然岸线呢
         196. 也是海洋督察
         197. 这个
-        198. 督察的一个重点
+        198. 督查的一个重点啊
+        },  
     }
     """
     system_prompt = f"{prompt}{example}"
@@ -212,12 +233,13 @@ def run(index, cut_lines):
         stream=False,
         response_format={'type': 'json_object'}
     )
-
+    print(response.choices[0].message.content)
     results = json.loads(response.choices[0].message.content)
 
-    before = results['before']
 
-    # 2. 遍历这个字典的键和值
+
+    # 1. 遍历before字典的键和值
+    before = results['before']
     print("--- 'before' 中的序号和内容 ---")
     before_lines = ""
     for key, value in before.items():
@@ -225,18 +247,36 @@ def run(index, cut_lines):
     print(before_lines)
 
 
+    # 2. 遍历error字典的键和值
+    error = results['error']
+    print("--- 'error' 中的序号和内容 ---")
+    error_lines = ""
+    for key, value in error.items():
+        error_lines = f"{error_lines}{key}: {value}\n"
+    print(error_lines)
 
+    # 3. 遍历maybe字典的键和值
+    maybe = results['maybe']
+    print("--- 'maybe' 中的序号和内容 ---")
+    maybe_lines = ""
+    for key, value in maybe.items():
+        maybe_lines = f"{maybe_lines}{key}: {value}\n"
+    print(maybe_lines)
+
+    # 4. 遍历after字典的键和值
     after = results['after']
-
-    # 2. 遍历这个字典的键和值
-    print("--- 'before' 中的序号和内容 ---")
+    print("--- 'after' 中的序号和内容 ---")
     after_lines = ""
     for key, value in after.items():
         after_lines = f"{after_lines}{key}: {value}\n"
     print(after_lines)
 
-    print(f"\n内容已成功写入 {file}.txt 文件。")
-    return f"\n已成功写入 {file}.txt 文件。", before_lines, after_lines
+
+    #保存结果
+    message= save_file(f"{file}修改建议.txt", f"{before_lines}{error_lines}{maybe_lines}{after_lines}")
+
+
+    return message, before_lines, error_lines, maybe_lines, after_lines, f"{file}修改建议.txt"
 
 
 def write_config_value(value):
